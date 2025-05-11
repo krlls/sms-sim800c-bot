de302cdf20a1f31b4c85df0c8561c9842416e1ba
Kirill Smirnov <krrllsm@gmail.com>
2025-05-11 15:11:38 +0300

    fix env

--- src/conf/env.ts
+++ src/conf/env.ts
@@ -2,6 +2,6 @@ import {protectEnv} from "../utils/protectEnv.ts";
 
 export const TELEGRAM_TOKEN = protectEnv(process.env.TELEGRAM_BOT_TOKEN,'TELEGRAM_BOT_TOKEN')
 export const CLIENTS = protectEnv(process.env.CLIENTS,'CLIENTS')
-export const DEBUG = process.env.DEBUG
+export const DEBUG = process.env.DEBUG === 'true'
 
 export const VENDOR = '1a86'
